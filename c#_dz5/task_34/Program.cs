f0ba15e35ad0810979bf3c554683e88110581a20
Vestibolog <tarn@ngs.ru>
2022-05-07 19:36:36 +0700

    ДЗ к семинару 5

---
+++ c#_dz5/task_34/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными
+положительными трёхзначными числами. Напишите
+программу, которая покажет количество чётных чисел в
+массиве.
+[345, 897, 568, 234] -> 2
+*/
+Console.WriteLine("Введите длину массива");
+
+int number = Int32.Parse(Console.ReadLine());
+
+int[] array = new int[number];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+int evenCol = 0;
+PrintArray(array);
+for (int i = 0; i < array.Length; i++)
+{
+    if ( array[i] %2 ==0) evenCol++;
+}
+Console.WriteLine($"Из них четных {evenCol}");
---
+++ c#_dz5/task_38/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. Найдите
+разницу между максимальным и минимальным
+элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+Console.WriteLine("Введите длину массива");
+
+int number = Int32.Parse(Console.ReadLine());
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[number];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+}
+PrintArray(array);
+
+int min = array[1];
+int max = array[1];
+for (int i = 0; i < array.Length; i++)
+{
+    if ( min > array[i] ) min = array[i];
+    if ( max < array[i] ) max = array[i];
+}
+Console.WriteLine($"min={min} max={max}  Ответ={min*max}");
