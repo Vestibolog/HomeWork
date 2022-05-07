b24e8ccae1aad53ba1774b2bcc1aa5096508f646
Vestibolog <tarn@ngs.ru>
2022-05-07 14:47:26 +0700

    ДЗ к семинуру 3

---
+++ c#_dz3/task_19/Program.cs
@@ -0,0 +1,37 @@
+﻿//--------------------task 19-------------------
+int number = 23432;
+
+int number_1 = number/10000;
+int number_2 = number/1000 % 10;
+int number_4 = (number % 100)/10;
+int number_5 = number % 10;
+
+Console.WriteLine($"Число =  {number} = {number_1} {number_2} _ {number_4} {number_5}");
+if ( (number_1 == number_5) & (number_2 == number_4) )
+{
+    Console.WriteLine($"Число =  {number} палиндромом");
+}else
+{
+    Console.WriteLine($"Число =  {number} НЕ палиндромом");
+}
+//--------------------task 21-------------------
+int pointA_x = 7;
+int pointA_y = -5;
+int pointA_z = 0;
+
+int pointB_x = 1;
+int pointB_y = -1;
+int pointB_z = 9;
+
+double result = Math.Sqrt(Math.Pow(pointA_x - pointB_x, 2) +
+                           Math.Pow(pointA_y - pointB_y, 2) +
+                           Math.Pow(pointA_z - pointB_z, 2));
+Console.WriteLine($"Ответ = {Math.Round(result, 2)}");
+//--------------------task 23-------------------
+int iterator = 5;
+Console.WriteLine($"Итератор = {iterator}");
+for ( int i = 1; i <= iterator; i++ )
+{
+    Console.Write($" {Math.Pow(i,3)}, ");
+}
+Console.WriteLine("");
---
+++ c#_dz3/task_21/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+int pointA_x = 7;
+int pointA_y = -5;
+int pointA_z = 0;
+
+int pointB_x = 1;
+int pointB_y = -1;
+int pointB_z = 9;
+
+double result = Math.Sqrt(Math.Pow(pointA_x - pointB_x, 2) +
+                           Math.Pow(pointA_y - pointB_y, 2) +
+                           Math.Pow(pointA_z - pointB_z, 2));
+Console.WriteLine($"Ответ = {Math.Round(result, 2)}");
